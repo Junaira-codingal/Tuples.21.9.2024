fbd2dba30f6068fc2abe7c522d7a0a6b6f8e1382
Junaira <advshakerullah@gmail.com>
2024-09-21 17:16:39 +0600

    flip flop

---
+++ Flip Flop.py
@@ -0,0 +1,23 @@
+print("\033c")
+
+def palind(r):
+    
+    end = len(r) -1
+    start = 0
+    
+    while(start<end):
+        
+        if(r[start] != r[end]):
+           return False
+    
+         
+        start= start + 1
+        end = end -1
+    return True
+
+r = (1,2,3,3,2,1)
+
+if(palind(r)):
+    print("The Tuple is flip-flop")
+else:
+    print("The Tuple is not flip-flop")
