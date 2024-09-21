f8432814c841622dc3d45fabc8c373c33458e8ff
Junaira <advshakerullah@gmail.com>
2024-09-21 17:03:06 +0600

    first commit

---
+++ Tuple operation.py
@@ -0,0 +1,11 @@
+print("\033c")
+tuplex = ("Tuple", False, 3.2, 1)
+print(tuplex)
+
+tuplex = tuplex + ("Junaira",)
+print(tuplex)
+
+print(    tuplex[4]   )
+
+tuple1 = (50,10,60,70,50,50,50)
+print(   tuple.count(50)   )
